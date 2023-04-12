492fc45e1728b1d309c9e81664b0a76fed86581c
Sultan Alfoory <sultan.alfoory@colorado.edu>
2023-04-12 13:20:41 -0700

    Pushing the base SQL script as a folder called init_data

---
+++ projectCode/init_data/create.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS userinfo CASCADE;
+CREATE TABLE IF NOT EXISTS userinfo (
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100),
+  username VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  city VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS passes CASCADE;
+CREATE TABLE IF NOT EXISTS passes (
+  pass_id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  location VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS locations CASCADE;
+CREATE TABLE IF NOT EXISTS locations (
+  location_id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS passes_to_locations;
+CREATE TABLE IF NOT EXISTS locations (
+    pass_id INT,
+    location_id INT
+);
